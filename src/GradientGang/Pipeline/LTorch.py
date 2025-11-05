0c486cc30268db6751d7fa4d876d9a9825729270
Lorenzo Pettenuzzo <lorenzo.pettenuzzo02@gmail.com>
2025-11-05 07:13:48 +0100

    Pipeline: added LTorch.__init__ method for architecture creation

--- src/GradientGang/Pipeline/LTorch.py
+++ src/GradientGang/Pipeline/LTorch.py
@@ -3,4 +3,10 @@ import torch.nn as nn
 import lightning as L
 
 class LTorch (L.LightningModule):
-    pass
+    def __init__(self, components: list, componentsDict: dict):
+        super().__init__()
+
+        self.arch = nn.Sequential()
+
+        for c in components:
+            self.arch.append(componentsDict[c[0]](**(c[1])))   # parameters are a dictionary decompressed
