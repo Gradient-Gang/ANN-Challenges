66b22acee9bff89adaffff7f598deb166e664ecd
Lorenzo Pettenuzzo <lorenzo.pettenuzzo02@gmail.com>
2025-11-05 07:24:36 +0100

    Pipeline: add LTorch TODOs

--- src/GradientGang/Pipeline/LTorch.py
+++ src/GradientGang/Pipeline/LTorch.py
@@ -8,6 +8,7 @@ class LTorch (L.LightningModule):
 
         self.arch = nn.Sequential()
 
+        # TODO: add check for types (only nn.Module allowed)
         for c in components:
             self.arch.append(componentsDict[c[0]](**(c[1])))   # parameters are a dictionary decompressed
     
@@ -17,5 +18,5 @@ class LTorch (L.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_pred = self.arch(x)
-
+        # TODO: add epoch logging, maybe must be handled by Optuna
         return self.loss(y_pred, y)
