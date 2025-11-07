f276e8535034452b43125b1163d203ef8f0f35e3
Lorenzo Pettenuzzo <lorenzo.pettenuzzo02@gmail.com>
2025-11-07 16:25:35 +0100

    Pipeline: abstract Pipeline basic implementation

--- src/GradientGang/Pipeline/Pipeline.py
+++ src/GradientGang/Pipeline/Pipeline.py
@@ -1,3 +1,12 @@
+"""
+hyper.yaml
+
+"param1":
+    "type": "categ" | "float" | "int"
+    ...
+"""
+
+import abc
 import torch
 import torch.nn as nn
 import lightning as L
@@ -5,15 +14,44 @@ import optuna
 import yaml
 from . import LTorch
 
-# implementation for a single architecture in the file
-
-class Pipeline:
-    # reads and stores architecture
+class Pipeline (abc.ABC):
     def __init__(self, train_data, val_data, test_data, path_arch: str, path_hyper: str):
+        self.arch = yaml.safe_load(path_arch)
+        self.hyper = yaml.safe_load(path_hyper)
+
+        self.train_data = train_data
+        self.val_data = val_data
+        self.test_data = test_data
+
+    # might build from arch, or restart from checkpoint?
+    @abc.abstractmethod
+    def build_architecture(self, arch: dict, params: dict) -> LTorch.AbstractLTorch:
         pass
+
+    def getParams(self, params: dict, trial: optuna.trial.BaseTrial):
+        vals = {}
+        
+        for k in params:
+            if params[k]["type"] == "categ":        # params: list of categories
+                vals[k] = trial.suggest_categorical(k, params[k]["seq"])
+            elif params[k]["type"] == "float":      # params: low, high, step, log
+                vals[k] = trial.suggest_float(k, params[k]["low"], params[k]["high"], params[k]["step"], params[k]["log"])
+            elif params[k]["type"] == "int":        # params: low, high, step, log
+                vals[k] = trial.suggest_int(k, params[k]["low"], params[k]["high"], params[k]["step"], params[k]["log"])
+            else:
+                raise TypeError("Pipeline.getParams: Invalid type for parameter")
+    
+        return vals
 
     def optimize(self):
-        pass
+        study = optuna.create_study()
+        study.optimize(self.objective)
 
-    def objective(self, trial:optuna.trial.BaseTrial):
-        pass
+        return study.best_params
+
+    def objective(self, trial: optuna.trial.BaseTrial):
+        params = self.getParams(self.hyper, trial)
+
+        arch = self.build_architecture(self.arch, params)
+
+        return arch.validation_step()
