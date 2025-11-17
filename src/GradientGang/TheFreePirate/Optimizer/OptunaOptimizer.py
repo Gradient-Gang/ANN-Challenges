d3728216a07a52bfbe3267f7da05cfd93f5868e8
Lorenzo Pettenuzzo <lorenzo.pettenuzzo02@gmail.com>
2025-11-17 18:55:23 +0100

    THE FREE PIRATE: MockTrial implementation

--- src/GradientGang/TheFreePirate/Optimizer/OptunaOptimizer.py
+++ src/GradientGang/TheFreePirate/Optimizer/OptunaOptimizer.py
@@ -1,7 +1,9 @@
 import optuna
-import torch
+import numpy as np
 from ..Piepeline.PiratePipeline import PiratePipeline
-from .HyperParameter.AbstractHyperParameter import AbstractHyperParameter
+from .HyperParameter.AbstractHyperParameter import AbstractHyperparameter
+
+from ..Utils import getRaise
 
 """
 phi = {
@@ -40,11 +42,28 @@ phi = {
 class OptunaOptimizer:
     database = "REMOVED_KEY"
 
+    class MockTrial:
+        def __init__(self, params, study):
+            self.params = params
+            self.study = study
+            self.storage = study._storage  # Access the actual storage
+            self._trial_id = study.best_trial._trial_id  # Use best trial's ID
+
+        def suggest_int(self, name, low, high, *, step: int, log = False):
+            return getRaise(self.params, name, "best_params in MockTrial")
+        
+        def suggest_categorical(self, name, choices):
+            return getRaise(self.params, name, "best_params in MockTrial")
+        
+        def suggest_float(self, name, low, high, *, step = None, log = False):
+            return getRaise(self.params, name, "best_params in MockTrial")    
+
     # setup section
     def __init__(self, name: str, dataParams, callbackParams, params):
         self.params = params
 
-        self.study = optuna.create_study(sampler=optuna.samplers.TPESampler(seed=0), storage=OptunaOptimizer.database)
+        #self.study = optuna.create_study(sampler=optuna.samplers.TPESampler(seed=0), storage=OptunaOptimizer.database)
+        self.study = optuna.create_study(sampler=optuna.samplers.TPESampler(seed=0), storage=None)
         self.pipeline = PiratePipeline(name, dataParams, callbackParams, seed = 42)
     
     # objective section
@@ -76,14 +95,14 @@ class OptunaOptimizer:
         self.pipeline.setupKfoldEvaluation(self.phi)
         results = self.pipeline.kFoldEvaluation(self.phi, [self._optuna_callback])
 
-        self.current_trial.set_user_attr("mean", torch.mean(results))
-        self.current_trial.set_user_attr("std", torch.std(results))
+        self.current_trial.set_user_attr("mean", np.mean(results))
+        self.current_trial.set_user_attr("std", np.std(results))
 
-        return torch.mean(results)
+        return np.mean(results)
 
     def _optuna_callback(self, results):
         fold_number = len(results)
-        self.current_trial.report(torch.mean(results), )
+        self.current_trial.report(np.mean(results), fold_number)
         self.current_trial.set_user_attr(f"fold_{fold_number}", results[-1])
 
     def _build_arch(self, params: dict):
@@ -92,9 +111,14 @@ class OptunaOptimizer:
         for (key, value) in params.items():
             if isinstance(value, dict):
                 phi[key] = self._build_arch(value)
-            elif issubclass(type(value), AbstractHyperParameter):
-                phi[key] = value.getValue(self.current_trial)
+            elif issubclass(type(value), AbstractHyperparameter):
+                phi[key] = value.getValue(self.current_trial, self)
             else:
                 phi[key] = value
         
         return phi
+    
+    def getBestModel(self):
+        self.current_trial = OptunaOptimizer.MockTrial(self.study.best_params, self.study)
+
+        return self._build_arch(self.params)
